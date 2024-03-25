570a74e97db60192e7363b0d5dc8b8d55c7943c5
amircka <amirchik541@gmail.com>
2024-03-25 13:51:53 +0600

    sdsd

--- Dz7_Amir_36_1/app.js
+++ Dz7_Amir_36_1/app.js
@@ -1,1 +1,18 @@
-//efejfnjenfjenfjenfjenfje
+function checkNumber() {
+    var userNumber = parseInt(document.getElementById('userInput').value);
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+        alert('Please enter a valid number between 1 and 100.');
+        return;
+    }
+
+    var generatedNumber = Math.floor(Math.random() * 100) + 1;
+    var hintElement = document.getElementById('hint');
+
+    if (userNumber < generatedNumber) {
+        hintElement.innerText = "The generated number is greater.";
+    } else if (userNumber > generatedNumber) {
+        hintElement.innerText = "The generated number is smaller.";
+    } else {
+        hintElement.innerText = "Congratulations! You guessed the number!";
+    }
+}
